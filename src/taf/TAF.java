8a46c9963b4d4a38e6f1a9aae725c1dea7860798
Mari Do <mariem.abdi2000@gmail.com>
2022-03-01 22:05:04 +0100

    FIRST

---
+++ src/taf/TAF.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taf;
+import java.sql.Connection;
+import java.sql.DriverManager;
+/**
+ *
+ * @author maryo
+ */
+public class TAF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // mettre le frame MDI visible 
+        MainFrame f=new MainFrame();
+        f.setVisible(true);
+        
+
+    }}
